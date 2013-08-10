ef22cac2f0f5dfa0cc72aa780253c8f4130fdf65
Jason Altschuler <jason.altschuler@gmail.com>
2013-08-10 16:50:42 -0400

    Removed external dependencies

--- src/CSVreader.java
+++ src/CSVreader.java
@@ -1,68 +1,97 @@
 /*************************************************************************
- * @author  Altschuler and Wu Lab
+ * @author Jason Altschuler
  * 
- * Reads CSV files 
- * 
- * TODO: Remove stupid comments. Remove main method. read(..) doesn't
- * throw graceful exceptions -- is this a problem? See call hierarchy
+ * PURPOSE: Read CSV files
  ************************************************************************/
 
-import au.com.bytecode.opencsv.CSVReader;
 import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
 
 public class CSVreader {
 
-   //"C:\\Users\\Steve\\Documents\\MATLAB\\test1.csv"
-   // FOR K-MEANS, USE ROWS = 3000, COLUMNS = 2.
    /**
-    * Reads double[][] from csv .txt file
+    * Reads double[][] from csv file
     */
-   public static double[][] read(String inFile, int rows, int columns) throws IOException {
+   public static double[][] read(String inFile, int rows, int columns) {
       if (rows <= 0 || columns <= 0)
          throw new IllegalArgumentException("Invalid dimensions");
 
-      CSVReader reader = new CSVReader(new FileReader(inFile));
+      BufferedReader bf = null;
       double[][] arr = new double[rows][columns];
 
-      String[] nextLine;
+      String line = "";
       int r = 0;
       int c;
 
-      while ((nextLine = reader.readNext()) != null) {
-         for (c = 0; c < columns; c++)
-            arr[r][c] = Double.parseDouble(nextLine[c]);
+      try {
+         bf = new BufferedReader(new FileReader(inFile));
+         
+         while ((line = bf.readLine()) != null) {
+            String[] x = line.split(",");
 
-         r++;
+            if (x.length != columns)
+               throw new IllegalArgumentException("File has invalid dimensions (columns)");
+         
+            for (c = 0; c < columns; c++)
+               arr[r][c] = Double.parseDouble(x[c] + "\t");
+            
+            r++;
+         }
+         
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } finally {
+         if (bf != null) {
+            try {
+               bf.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
+         }
       }
 
-      //		if (r != rows)
-      //	       throw new IllegalArgumentException("File has invalid dimensions (rows)");
-
-      reader.close();
+      if (r != rows)
+         throw new IllegalArgumentException("File has invalid dimensions (rows)");
 
       return arr;
    }   
 
-   /**
-    * Unit testing
-    */
    public static void main(String[] args) {
-      try {
-         double[][] test = CSVreader.read("C:\\Users\\Jason\\Desktop\\Programming\\Matlab\\UT Southwestern work\\test1.csv", 200, 2);
+      String testFile;
+      int rows;
+      int columns;
+      boolean print = false;
 
+      // read user's configurations for test
+      if (args.length == 4) {
+         testFile = args[0];
+         rows = Integer.parseInt(args[1]);
+         columns = Integer.parseInt(args[2]);
+         print = Boolean.parseBoolean(args[3]);
+      }
+
+      // default test case
+      else {
+         testFile = "ImageForOtsus.csv";
+         rows = 1040;
+         columns = 1392;
+         print = false;
+      }
+
+      double[][] test = CSVreader.read(testFile, rows, columns);
+      
+      if (print) {
          for (int i = 0; i < test.length; i++) {
             for (int j = 0; j < test[0].length; j++)
                System.out.print(test[i][j] + " ");
             System.out.println();
          }
-
-      } catch (IOException e) {
-         e.printStackTrace();
       }
-
-      System.out.println("completed");
+      
+      System.out.println("Read " + testFile + " successfully!");
    }
-   
 }
